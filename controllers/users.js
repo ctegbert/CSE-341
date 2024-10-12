0afc67993fb111dc827a78d3f1c95257b7e7ccdd
Colton Egbert <145735468+ctegbert@users.noreply.github.com>
2024-10-11 21:08:49 -0500

    Rest_Add

--- controllers/users.js
+++ controllers/users.js
@@ -19,7 +19,53 @@ const getSingle = async (req, res) => {
     });
 };
 
+const createUser = async (req, res) => {
+    const user = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        favoriteColor: req.body.favoriteColor,
+        birthday: req.body.birthday
+    };
+    const response = await mongodb.getDatabase().db().collection('users').insertOne(user);
+    if (response.acknowledged) {
+        res.status(204).send();
+    } else {
+        res.status(500).json(response.error || 'An error of drastic proportions occured while adding the user!');
+    }
+};
+
+const updateUser = async (req, res) => {
+    const userId = new ObjectId(req.params.id);
+    const user = {
+        user: req.body.user,
+        email: req.body.email,
+        name: req.body.name,
+        ipaddress: req.body.ipaddress,
+    };
+    const response = await mongodb.getDatabase().db().collection('users').replaceOne({id: userId }, user);
+    if (response.modifiedCount > 0) {
+        res.status(204).send();
+    } else {
+        res.status(500).json(response.error || 'An error of drastic proportions occured while updating the user!');
+    }
+};
+
+const deleteUser = async (req, res) => {
+    const userId = new ObjectId(req.params.id);
+    const response = await mongodb.getDatabase().db().collection('users').deleteOne({id: userId });
+    if (response.deletedCount > 0) {
+        res.status(204).send();
+    } else {
+        res.status(500).json(response.error || 'An error of drastic proportions occured while deleting the user!');
+    }
+};
+
+
 module.exports = {
     getAll,
-    getSingle
+    getSingle,
+    createUser,
+    updateUser,
+    deleteUser
 };
